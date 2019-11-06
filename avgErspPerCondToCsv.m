6a22badab1a577f57175edfb012b97c517dc7e3d
Vyom Raval <47093961+vmr160030@users.noreply.github.com>
2019-11-06 13:08:21 -0600

    Added check for repeats in cElecs

--- avgErspPerCondToCsv.m
+++ avgErspPerCondToCsv.m
@@ -10,14 +10,28 @@
 
 % TODO: Make this a function that takes inputs
 
+%%%%%%%%%%%%%%%%%%%%%%%% USER INPUT ZONE BEGINS %%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Variable prefixes indicate data type
-strOutputCsv = 'K:\Dept\CallierResearch\Maguire\RA Folders\Vyom\ManjuTest.csv'; % Path to file you want to save
+strOutputCsv = 'K:\Dept\CallierResearch\Maguire\RA Folders\Vyom\ManjuTest_norepeats.csv'; % Path to file you want to save
 arrTimeRange = [0 4000];
 arrFreqRange = [9 12];
 cElecs = {'FP1', 'FPZ', 'AF3', 'F7', 'F5', 'F3', 'F1', 'FZ',...
-    'F2', 'F4', 'FT7', 'F5', 'F3', 'F1', 'FZ', 'F2', 'F4', 'FT7',...
-    'FCZ', 'FC2', 'CZ', 'C2', ' Tp8', 'P2', 'P4', 'P6', 'P8', 'CB1',...
-    'PO7', 'PO5', 'POZ', 'PO4', 'PO6', 'PO8', 'CB2', 'O1', 'OZ', 'O2'};
+          'F2', 'F4', 'FT7', 'FCZ', 'FC2', 'CZ', 'C2', ' Tp8',...
+          'P2', 'P4', 'P6', 'P8', 'CB1','PO7', 'PO5', 'POZ', 'PO4',...
+          'PO6', 'PO8', 'CB2', 'O1', 'OZ', 'O2'}; % Ensure that this has no repeats
+%%%%%%%%%%%%%%%%%%%%%%%% USER INPUT ZONE ENDS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Check for repeats
+bArrRepeat = size(cElecs) ~= size(unique(cElecs));
+if bArrRepeat(2)
+    disp('Please make sure cElecs has no repeats');
+    [D,~,X] = unique(cElecs(:));
+    Y = hist(X,unique(X));
+    Z = struct('name',D,'freq',num2cell(Y(:)));
+    disp('The following were repeated');
+    disp(D(Y>1));
+    return;
+end
 
 %Compute ersp with std_erspplot
 STUDY = pop_statparams(STUDY, 'groupstats','on','condstats','on',...
